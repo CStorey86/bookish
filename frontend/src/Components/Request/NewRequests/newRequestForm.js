fc19ba85a72fd3aa437b7ef36ca334c1804a666f
Claire Storey <clairestorey30614@gmail.com>
2022-05-26 18:00:06 +0100

    Completed addNewRequest form

--- frontend/src/Components/Request/NewRequests/newRequestForm.js
+++ frontend/src/Components/Request/NewRequests/newRequestForm.js
@@ -1,60 +1,63 @@
 import React,{Component,useState} from 'react';
-import Select from 'react-select';
 import './newRequests.css';
 import axios from 'axios';
 
-const options=[
-  { value: 'Book', label: 'Book'},
-  { value: 'Audiobook', label: 'Audio Book'},
-  { value: 'Braille', label: 'Braille'},
-];
+// const options=[
+//   { value: 'Book', label: 'Book'},
+//   { value: 'Audiobook', label: 'Audio Book'},
+//   { value: 'Braille', label: 'Braille'},
+// ];
 
 // If currency option added, extra function will be needed for currency conversion.
 
 function NewRequest () {
 
-  const [format, setFormat] = useState("");
-  const [title, setTitle] = useState("");
-  const [author, setAuthor] = useState("");
-  const [publisher, setPublisher] = useState("");
-  const [year, setYear] = useState("");
-  const [price, setPrice] = useState("");
+  const [formatChoice, setFormat] = useState("");
+  const [titleChoice, setTitle] = useState("");
+  const [authorChoice, setAuthor] = useState("");
+  const [publisherChoice, setPublisher] = useState("");
+  const [yearChoice, setYear] = useState("");
+  const [priceChoice, setPrice] = useState("");
+
 
   //Error Message
     // const [errMsg, setErrMsg] = useState("");
 
+  async function handleSubmit(event) {
+    event.preventDefault();
 
-    async function handleSubmit(event) {
-        event.preventDefault();
+    const url='http://localhost:4000/requests'; 
 
-        const url='http://localhost:4000/requests'; 
+    axios.post(url,{
+      title: titleChoice,
+      author: authorChoice,
+      publisher: publisherChoice,
+      year: yearChoice,
+      price: priceChoice,
+      format: formatChoice,
+      status: "Awaiting Allocation",
+      dateStatusChange: Date.now()
+    })
+    .then (res =>{
+        console.log("New Request Added");
+    })
+  }
 
-        axios.post(url,{
-          format: format,
-          title: title,
-          author: author,
-          publisher: publisher,
-          year: year,
-          price: price,
-          status: "Awaiting Allocation",
-          dateStatusChange: Date.now()
-        })
-        .then (res =>{
-            console.log("New Request Added");
-        })
-    }
-
-    return (  
+  return (  
       <div className="newRequestForm">
         <h2>NEW REQUEST </h2>
           <form onSubmit={handleSubmit}>
-           <div className="formMain">
-              <div className ="formRow">
+            <div className="formMain">
+                <div className ="formRow">
                     <div className="formSection">
                       <label htmlFor="format">Format:</label>
-                      <Select id="format" name="format" placeholder="Select Format" options = {options} required
-                        // value={format} onChange={(e)=>setFormat(e.target.value)}
-                      />        
+                      <select id="format" name="format" placeholder="Select Format" 
+                          value={formatChoice} onChange={(e)=>setFormat(e.target.value)}>
+                        <option value={"default"} disabled> Select Format </option>
+                        <option value={"Book"}>Book</option>
+                        <option value={"Audio Book"}>Audio Book</option>
+                        <option value={"Braille"}>Braille</option>    
+                      </select>
                     </div>                    
                 </div>
 
@@ -62,12 +65,12 @@ function NewRequest () {
                     <div className="formSection">
                         <label htmlFor="format">Title:</label>
                         <input id="title" name="title" type="text"
-                          value={title} onChange={(e)=>setTitle(e.target.value)} />  
+                          value={titleChoice} onChange={(e)=>setTitle(e.target.value)} required/>  
                     </div>
                     <div className="formSection">
                         <label htmlFor="format">Author:</label>
                         <input id="author" name="author" type="text"
-                          value={author} onChange={(e)=>setAuthor(e.target.value)} /> 
+                          value={authorChoice} onChange={(e)=>setAuthor(e.target.value)} required/> 
                     </div>                    
                 </div>
 
@@ -75,18 +78,18 @@ function NewRequest () {
                     <div className="formSection">
                         <label htmlFor="format">Publisher:</label>
                         <input id="publisher" name="publisher" type="text"
-                            value={publisher} onChange={(e)=>setPublisher(e.target.value)} /> 
+                            value={publisherChoice} onChange={(e)=>setPublisher(e.target.value)}/> 
                     </div>
                     <div className="formSectionA">
                         <label htmlFor="format">Year:</label>
                         <input id="year" name="year" type="number" min="1900" max="2030"
-                          value={year} onChange={(e)=>setYear(e.target.value)} />  
+                          value={yearChoice} onChange={(e)=>setYear(e.target.value)} />  
                     </div>
                     <div className="formSectionB">
                         <label htmlFor="format">Price (£):</label>
                         {/* Drop down for currency option could be added here later */}
                         <input id="price" name="price" min="0" step="0.01" type="number"
-                          value={price} onChange={(e)=>setPrice(e.target.value)} />          
+                          value={priceChoice} onChange={(e)=>setPrice(e.target.value)} required/>          
                     </div>
                 </div>
 
@@ -94,20 +97,9 @@ function NewRequest () {
                     <input type="submit" value="SUBMIT REQUEST" id="submitFormBtn"/>
                     {/* <button id="submitFormBtn" type="submit">SUBMIT REQUEST</button> */}
                 </div>
-              </div>
-
-
-
-    
-
-
-
-              
+            </div>
           </form>
-
- 
       </div>
-
     );
   }
 
