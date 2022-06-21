12d54d852ac0609c4b0cd126f50509646179dd8b
Claire Storey <clairestorey30614@gmail.com>
2022-06-21 22:36:38 +0100

    reset errors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,29 +14,24 @@ import Authorisations from './Components/Request/Authorisations/Authorisations';
 import Users from './Components/Users/Users';
 import AllRequests from './Components/Request/AllRequests';
 
-
-
-
 //DEV PROCESS - REMOVE AFTER LOGIN CONFIRMED
-let userID = "62860fe823c9c0ba976b9ba7"; // STANDARD
-
-const user = GetUserFromID(userID);
-// let userID= "62860fe823c9c0ba976b9ba6"; // EMPLOYEE
-
-// let userID= "62860fe823c9c0ba976b9ba5"; // AUTHORISER
-
+let thisUser ={
 
   //STANDARD USER
-    // id: "62860fe823c9c0ba976b9ba7",
-    // firstName: "Claire",
-    // lastName: "Storey",
-    // isAdmin: false,
-    // isEmployee: false,
+    id: "62860fe823c9c0ba976b9ba7",
+    firstName: "Claire",
+    lastName: "Storey",
+    email: "claire@email.com",
+    createdDate: "2022-05-05T13:10:00.000+00:00",
+    isAdmin: false,
+    isEmployee: false,
 
   // EMPLOYEE
     // id: "62860fe823c9c0ba976b9ba6",
     // firstName: "Basil",
     // lastName: "Bush",
+    // email: "basil@email.com",
+    // createdDate: "2022-05-05T13:10:00.000+00:00",
     // isAdmin: false,
     // isEmployee: true,
     // authLimit: 50.00,
@@ -45,9 +40,11 @@ const user = GetUserFromID(userID);
     // id: "62860fe823c9c0ba976b9ba5",
     // firstName: "Angela",
     // lastName: "Jones",
+    // email: "angela@email.com",
+    // createdDate: "2022-05-05T13:10:00.000+00:00",
     // isAdmin: true,
     // isEmployee: false,
-//}
+}
 
 
 class App extends Component {
@@ -62,19 +59,19 @@ class App extends Component {
               <Route path="/Register" element={<Register />} />
 
               {/* Routes below are only accessible with valid login */}
-              <Route path="/Home" element={<Home user={user}/>}/>
-              <Route path="/Requests" element={<Requests user={user}/>}/>
-              <Route path="/Profile" element={<Profile user={user}/>} />
-              <Route path="/NewRequest" element={<NewRequest user={user}/>} />
+              <Route path="/Home" element={<Home user={thisUser}/>}/>
+              <Route path="/Requests" element={<Requests user={thisUser}/>}/>
+              <Route path="/Profile" element={<Profile user={thisUser}/>} />
+              <Route path="/NewRequest" element={<NewRequest user={thisUser}/>} />
 
               {/* Employee */}
-              <Route path="/Allocations" element={<Allocations user={user}/>} />
-              <Route path="/AllRequests" element={<AllRequests user={user}/>} />
+              <Route path="/Allocations" element={<Allocations user={thisUser}/>} />
+              <Route path="/AllRequests" element={<AllRequests user={thisUser}/>} />
               
 
               {/* Authoriser */}
-              <Route path="/Authorisations" element={<Authorisations user={user}/>} />
-              <Route path="/Users" element={<Users user={user}/>} />
+              <Route path="/Authorisations" element={<Authorisations user={thisUser}/>} />
+              <Route path="/Users" element={<Users user={thisUser}/>} />
 
 
             </Routes>
