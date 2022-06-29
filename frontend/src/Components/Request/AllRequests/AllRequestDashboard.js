79b18bb24adba2ee5652b929b79cd93aee411a42
Claire Storey <clairestorey30614@gmail.com>
2022-06-29 06:15:36 +0100

    update

--- frontend/src/Components/Request/AllRequests/AllRequestDashboard.js
+++ frontend/src/Components/Request/AllRequests/AllRequestDashboard.js
@@ -11,7 +11,7 @@ class AllRequestDashboard extends React.Component{
         super(props);
         this.state={
             requests: [],
-            notCompRequests: [],
+            compRequests: [],
             currentRequest: {},
             currentRequestID:"",
             userID: props.currentUser._id,   
@@ -21,8 +21,8 @@ class AllRequestDashboard extends React.Component{
     }
 
     componentDidMount(){
-        const url= `/requests/`; 
-        const notCompUrl = `/requests/?status!=${"Complete"}`;
+        const url= `/requests/?status!=Complete`; 
+        const compUrl = `/requests/?status=Complete`;
 
         axios.get(url)
         .then((Response) => {
@@ -30,21 +30,22 @@ class AllRequestDashboard extends React.Component{
           this.setState({
             requests: Response.data     
           })
+          axios.get(compUrl)
+          .then((Response) => {
+            //set notCompRequests[] as data recieved from api call
+            this.setState({
+              compRequests: Response.data     
+            })
+          })
+          .catch((error) => {
+            console.log(error);
+          });
         })
         .catch((error) => {
           console.log(error);
         });
 
-        axios.get(notCompUrl)
-        .then((Response) => {
-          //set notCompRequests[] as data recieved from api call
-          this.setState({
-            notCompRequests: Response.data     
-          })
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+ 
     };
 
     updateCurrentRequest(item){
@@ -61,7 +62,8 @@ class AllRequestDashboard extends React.Component{
             <div>
                 <div className="LeftPanel">
                     <h2>ALL REQUESTS </h2>
-                    <RequestListAll activeRequests={this.state.notCompRequests} requests={this.state.requests} updateCurrentRequest={this.updateCurrentRequest} />
+                    <RequestListAll completeRequests={this.state.compRequests} 
+                      requests={this.state.requests} updateCurrentRequest={this.updateCurrentRequest} />
                 </div>
 
                 <div className="RightPanel">
